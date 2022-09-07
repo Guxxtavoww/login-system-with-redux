a268b4890ab279e9aff12830afe64903d016d184
Guxxtavoww <gustavoaugustocar@hotmail.com>
2022-09-07 16:00:24 -0300

    Iniciando a estrutura do projeto

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.querySelector("div#root") as HTMLElement);
+
+root.render (
+    <App />
+);
